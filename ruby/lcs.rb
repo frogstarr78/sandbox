18403a797b6c12186182c3063be33622f5ddf46e
Scott Noel-Hemming <frogstarr78@gmail.com>
2009-10-20 06:28:08 -0700

    Cleaned up

--- ruby/lcs.rb
+++ ruby/lcs.rb
@@ -4,53 +4,6 @@ require 'colored'
 require 'test/unit'
 require 'test/unit/assertions'
 
-seq1 = %w(a b c e h j l m n p) 
-seq2 = %w(b c d e f j k l m r s t) 
-
-puts seq1.inspect, seq2.inspect
-
-lcs = Diff::LCS.LCS(seq1, seq2) 
-puts 'lcs ' << lcs.inspect
-diffs = Diff::LCS.diff(seq1, seq2) 
-#puts 'diffs ' << diffs.inspect
-sdiff = Diff::LCS.sdiff(seq1, seq2) 
-#puts 'sdiff ' << sdiff.inspect
-
-puts '-'*10
-r = []
-Diff::LCS.sdiff(seq1, seq2) {|diff| 
-  action = diff.action == '=' ? ' ' : diff.action
-  color  = action == '-' ? :red : :green
-  left   = diff.new_element || ' '
-  right  = diff.old_element || ' '
-  r << [left.send(color), action, right.send(color)]
-}
-puts r.inspect
-t = r.transpose
-puts t.inspect
-
-puts '-'*10
-puts t[1]*' '
-puts (t[0])*' '
-puts (t[2])*' '
-puts '-'*10
-#t.each {|row|
-#  puts row * ' '
-#}
-
-[
-  %w(a b c e h j l m n p),
-  %w(b c d e f j k l m r s t) 
-]
-puts %q(
-+     -   !   -   *
-a b c   e h j   l m n p
-  b c d e f j k l m r s t
--     +   !   +   *
-)
-
-#include Test::Unit::Assertions
-
 class MyTest < Test::Unit::TestCase
   def assert_equal_diffs string1, string2
     if string1 != string2
@@ -80,9 +33,28 @@ class MyTest < Test::Unit::TestCase
     end
   end
 
-  def test_me
+  def test_me_failing
     seq1 = %w(a b c e h j l m n p) 
     seq2 = %w(b c d e f j k l m r s t) 
-    assert_equal_diffs seq1, seq2
+    begin
+      assert_equal_diffs seq1, seq2
+    rescue Test::Unit::AssertionFailedError
+    end
+  end
+
+  def test_me_failing_again
+    seq1 = %w(a b c e h j l m n p) 
+    seq2 = %w(b c d e f j k l m r s t) 
+    assert_raises Test::Unit::AssertionFailedError do
+      assert_equal_diffs seq1, seq2
+    end
+  end
+
+  def test_no_passing
+    seq1 = %w(a b c e h j l m n p) 
+    seq2 = %w(a b c e h j l m n p) 
+    assert_nothing_raised {
+      assert_equal_diffs seq1, seq2
+    }
   end
 end
