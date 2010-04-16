4bd121ca551227bd6905538415dd7ba2c837352e
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-04-16 04:03:02 -0700

    Adding example of how to parse simple sentences using treetop and datamapper

---
+++ ruby/pratt_dsl_orm.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'dm-core'
+require 'pratt_dsl'
+
+DataMapper.setup :default, "sqlite3:sample.sqlite3"
+
+class Project
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, :required => true
+
+  def self.named name
+    first :name => name
+  end
+end
+
+DataMapper.auto_migrate!
+
+proj = Project.new
+proj.attributes = { :name => 'Lunch/Break' }
+proj.save
+
+proj = Project.new
+proj.attributes = { :name => 'Research' }
+proj.save
+
+module PrattDsl
+  class OrmLookup < Treetop::Runtime::SyntaxNode
+
+    def to_o
+      Project.named text_value
+    end
+  end
+end
---
+++ ruby/pratt_dsl_test.rb
@@ -0,0 +1,54 @@
+require 'rubygems'
+require 'shoulda'
+require 'treetop'
+require 'pratt_dsl_orm'
+
+
+Treetop.load "pratt_dsl"
+
+class PrattDslTest < Test::Unit::TestCase
+  context PrattDslParser do
+    setup do
+      @parser = PrattDslParser.new
+    end
+
+    context 'simple sentences beginning with some expected verbs' do
+      sentences = [
+        'begin Lunch/Break at 12:00.',
+        'Begin Lunch/Break at 12:00.',
+        'start Lunch/Break at 12:00.',
+        'Start Lunch/Break at 12:00.',
+        'restart Lunch/Break at 12:00.',
+        'Restart Lunch/Break at 12:00.',
+        'cease Lunch/Break at 12:00.',
+        'Cease Lunch/Break at 12:00.',
+        'end Lunch/Break at 12:00.',
+        'End Lunch/Break at 12:00.',
+      ].each do |sentence|
+        should "parse sentence #{sentence}" do
+          assert @parser.parse(sentence)
+        end
+      end
+    end
+
+    context 'simple sentences with conjuctions' do
+      sentences = [
+        'begin Lunch/Break at 12:00 and end Lunch/Break at 1:00.',
+        'Begin Lunch/Break at 12:00 and End Lunch/Break at 1:00.',
+      ].each do |sentence|
+        should "parse sentence #{sentence}" do
+          parsed = @parser.parse(sentence)
+          assert parsed
+          puts parsed.fragment.project.inspect
+        end
+      end
+    end
+
+    context "doesn't parse sentences with or in them" do
+        sentence = 'begin Lunch/Break at 12:00 or end Lunch/Break at 1:00.'
+        should "not parse #{sentence}" do
+          assert !@parser.parse(sentence)
+        end
+    end
+  end
+end
