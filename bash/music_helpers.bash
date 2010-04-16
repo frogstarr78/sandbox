a38773f73ee421f404560f630b73d38feea2563b
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-04-16 13:52:39 -0700

    Adding music helper functions in a bash script

---
+++ bash/music_helpers.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function clean_file () {
+	local file_name=$1
+	echo $file_name | tr ' ' '_' | sed 's/_-_/-/'
+}
+
+function title () {
+	local file_name=$1
+	echo $file_name | cut -d '-' -f 1
+}
+
+function track () {
+	local file_name=$1
+	echo $file_name | cut -d '.' -f 1 | cut -d '-' -f 2
+}
+
+function strip_part () {
+	local strip_off=$1
+	local file_name=$2
+	echo $file_name | sed s/$strip_off//
+}
+
+function dasherize_track_and_title () {
+	local file_name=$1
+	echo $file_name | sed 's/\(..\)_/\1-/'
+}
