5546c5cc79d46871cdff1a838784e294df1cce62
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-04-16 16:14:15 -0700

    Adding ogg2mp3 conversion function

--- bash/music_helpers.bash
+++ bash/music_helpers.bash
@@ -1,27 +1,60 @@
 #!/bin/bash
 
 function clean_file () {
-	local file_name=$1
-	echo $file_name | tr ' ' '_' | sed 's/_-_/-/'
+	local files=$1
+	for f in $files
+	do
+		file_name=`basename $f`
+		mv $f `echo $file_name | tr ' ' '_' | sed 's/_-_/-/'`
+	done
 }
 
 function title () {
 	local file_name=$1
+	file_name=`basename $file_name`
 	echo $file_name | cut -d '.' -f 1 | cut -d '-' -f 2 | tr '_' ' '
 }
 
 function track () {
 	local file_name=$1
+	file_name=`basename $file_name`
 	echo $file_name | cut -d '-' -f 1
 }
 
 function strip_part () {
 	local strip_off=$1
-	local file_name=$2
-	echo $file_name | sed s/$strip_off//
+	local files=$2
+	for f in $files
+	do
+		file_name=`basename $f`
+		mv $f `echo $file_name | sed "s/$strip_off//"`
+	done
 }
 
 function dasherize_track_and_title () {
-	local file_name=$1
-	echo $file_name | sed 's/\(..\)_/\1-/'
+	local files=$1
+	for f in $files
+	do
+		file_name=`basename $f`
+		mv $f `echo $file_name | sed 's/\(..\)_/\1-/'`
+	done
+}
+
+function set_track_and_title () {
+	local files=$1
+	for f in $files
+	do
+		mp3info -n `track $f` -t "`title $f`" $f
+	done
+}
+
+function ogg2mp3 () {
+	for f in *.ogg
+	do
+		fname=`basename $f .ogg`
+		twolame $f $fname.mp2
+		lame $fname.mp2 $fname.mp3
+		rm $fname.ogg
+		rm $fname.mp2
+	done
 }
