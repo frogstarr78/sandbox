fb6f528015f95e73ce0db720a5d0a90dc849ea6c
Scott Noel-Hemming <frogstarr78@gmail.com>
2010-05-13 13:10:21 -0700

    Got Conditionals working finally.

--- ruby/gen-lang/a.rb
+++ ruby/gen-lang/a.rb
@@ -1,4 +1,5 @@
 require 'generator' 
+require 'delegate'
 require 'rubygems'
 require 'ruby-debug'
 
@@ -46,18 +47,6 @@ class String
   end
 end
 
-class StackTooSmall < Exception
-  def message
-    "Not enough items on stack"
-  end
-end
-
-class UnexpectedEOI < Exception
-  def message
-    "Unexpected end of input"
-  end
-end
-
 class ScratchLexer
   attr_accessor :words, :generator
   def initialize txt
@@ -70,7 +59,6 @@ class ScratchLexer
     word = nil
 
     while char = @words.read(1)
-      puts "nw char '#{char}'" if DEBUG
       if char.is_whitespace?
         if word.blank?
           next
@@ -88,7 +76,6 @@ class ScratchLexer
   def next_chars_to up_tochar
     word = nil
     while char = @words.read( 1 )
-      puts "nct char '#{char}'" if DEBUG
       if char == up_tochar
         return word
       else
@@ -117,21 +104,40 @@ class ScratchLexer
       word << char
       char = generator.next
     end
-    puts "word #{word}" if DEBUG
 
     word
   end
 end
 
 class Scratch
+  class StackTooSmall < Exception
+    def initialize items, expected
+      super( "Not enough items on stack:
+      Expected: #{expected.inspect} items but stack is #{items.inspect}"
+           )
+    end
+  end
+
+  class UnexpectedEOI < Exception
+    def message
+      "Unexpected end of input"
+    end
+  end
+
+  class MissingListExpectation < Exception
+    def initialize received
+      super "List expected, received instead '#{received.class}'"
+    end
+  end
+
   attr_accessor :dictionary, :stack, :buffer, :data_stack, :lexer, :latest
-  IMMEDIATES = %w(VAR CONST " /* DEF END)
+  IMMEDIATES = %w(VAR CONST " /* DEF END [)
 
   def initialize
     @dictionary = {}
     @buffer     = []
     @data_stack = []
-    @stack      = @data_stack
+    @stack      = SimpleDelegator.new @data_stack
     @lexer      = nil
   end
 
@@ -145,8 +151,39 @@ class Scratch
     dictionary.update word.upcase => code
   end
 
-  def define_constant value, terp
-    lambda {|terp| terp.stack << value }
+  def make_word code
+    lambda do |terp|
+      code.each do |word|
+        terp.interpret word
+      end
+    end
+  end
+
+  def start_compiling
+    self.stack.__setobj__ self.buffer
+  end
+
+  def stop_compiling
+    self.stack.__setobj__ self.data_stack
+  end
+
+  def compiling?
+    self.stack == self.buffer
+  end
+
+  def run text
+    self.lexer = ScratchLexer.new(text)
+
+    while word = lexer.next_word
+      token = self.compile word
+      if IMMEDIATES.include? word
+        self.interpret token
+      elsif self.compiling?
+        self.stack << token
+      else
+        self.interpret token
+      end
+    end
   end
 
   def compile word
@@ -160,35 +197,6 @@ class Scratch
     end
   end
 
-  def start_compiling
-    self.stack = self.buffer
-  end
-
-  def stop_compiling
-    self.stack = self.data_stack
-  end
-
-  def compiling?
-    self.stack == self.buffer
-  end
-
-  def run text
-    self.lexer = ScratchLexer.new(text)
-
-    while word = lexer.next_word
-      token = self.compile word
-      puts "word '#{word}'" if DEBUG
-      if IMMEDIATES.include? word
-        puts "word '#{word}'" if DEBUG
-        self.interpret token
-      elsif self.compiling?
-        self.stack << token
-      else
-        self.interpret token
-      end
-    end
-  end
-
   def interpret word
     if word.is_a? Proc
       word.call self
@@ -196,11 +204,25 @@ class Scratch
       self.stack << word
     end
   end
+
+  def error_if_stack_isnt_sufficient! check, arg = nil
+    case check
+    when :empty?
+      raise StackTooSmall.new stack, '> 0' if self.stack.empty? 
+    when :<
+      raise StackTooSmall.new stack, arg if self.stack.size < arg 
+    end
+  end
 end
 
 PrintingWords = {
   "PRINT" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.empty? 
+    terp.error_if_stack_isnt_sufficient! :empty?
+    print terp.stack.pop
+  end,
+
+  "PUTS" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :empty?
     puts terp.stack.pop
   end,
 
@@ -209,9 +231,11 @@ PrintingWords = {
   end
 
 }
+PrintingWords["."] = PrintingWords["PRINT"]
+PrintingWords["!"] = PrintingWords["PUTS"]
 
 math_op = lambda do |terp, op|
-  raise StackTooSmall.new if terp.stack.size < 2
+  terp.error_if_stack_isnt_sufficient! :<, 2
   tstack = terp.stack.pop
   tstack2 = terp.stack.pop
   terp.stack << tstack2.send( op, tstack )
@@ -235,7 +259,7 @@ MathWords = {
   end,
 
   "√"  => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.empty?
+    terp.error_if_stack_isnt_sufficient! :empty?
     tstack = terp.stack.pop
     terp.stack << tstack ** 0.5
   end,
@@ -251,14 +275,14 @@ VariableWords = {
   end,
 
   "STORE" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 2
+    terp.error_if_stack_isnt_sufficient! :<, 2
     @var = terp.stack.pop
     new_val = terp.stack.pop
     @var.value = new_val
   end,
 
   "FETCH" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 1
+    terp.error_if_stack_isnt_sufficient! :<, 1
     @var = terp.stack.pop
     terp.stack << @var.value
   end
@@ -267,7 +291,7 @@ VariableWords = {
 
 ConstantWords = {
   "CONST" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 1
+    terp.error_if_stack_isnt_sufficient! :<, 1
 
     const_name = terp.lexer.next_word
     raise UnexpectedEOI.new if const_name.nil?
@@ -280,7 +304,6 @@ ConstantWords = {
 StringWords = {
   '"' => lambda do |terp|
     word = terp.lexer.next_chars_to('"')
-    puts "word '#{word}'" if DEBUG
     terp.stack << word
   end
 }
@@ -309,31 +332,25 @@ CompilingWords = {
   "END" => lambda do |terp|
     code = terp.stack.dup
     terp.stack = []
-    terp.define_variable(
-      terp.latest, lambda do |terp|
-        code.each do |word|
-          terp.interpret word
-        end
-      end
-    )
+    terp.define_variable terp.latest, terp.make_word( code )
     terp.stop_compiling
   end
 }
 
 StackWords = {
   "DUP" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 1
+    terp.error_if_stack_isnt_sufficient! :<, 1
     tos = terp.stack.pop
     terp.stack << tos << tos
   end,
 
   "DROP" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 1
+    terp.error_if_stack_isnt_sufficient! :<, 1
     terp.stack.pop
   end,
 
   "SWAP" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 2
+    terp.error_if_stack_isnt_sufficient! :<, 2
     tos = terp.stack.pop
     _2os = terp.stack.pop
     terp.stack << tos
@@ -341,7 +358,7 @@ StackWords = {
   end,
 
   "OVER" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 2
+    terp.error_if_stack_isnt_sufficient! :<, 2
     tos = terp.stack.pop
     _2os = terp.stack.pop
     terp.stack << _2os
@@ -350,7 +367,7 @@ StackWords = {
   end,
 
   "ROT" => lambda do |terp|
-    raise StackTooSmall.new if terp.stack.size < 3
+    terp.error_if_stack_isnt_sufficient! :<, 3
     tos = terp.stack.pop
     _2os = terp.stack.pop
     _3os = terp.stack.pop
@@ -360,6 +377,96 @@ StackWords = {
   end,
 }
 
+ListWords = {
+  "[" => lambda do |terp|
+    list = []
+    old_stack = terp.stack.__getobj__
+    terp.stack.__setobj__ list
+
+    loop do
+      word = terp.lexer.next_word
+      raise UnexpectedEOI.new if word.nil?
+      break if word == ']'
+
+      token = terp.compile word
+      if Scratch::IMMEDIATES.include? word
+        terp.interpret token
+      else
+        terp.stack << token
+      end
+    end
+
+    terp.stack.__setobj__ old_stack
+    terp.stack << list
+  end, 
+
+  "LENGTH" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 1
+    list = terp.stack.pop
+    terp.stack << list.size
+  end, 
+
+  "ITEM" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 2
+
+    index = terp.stack.pop
+    list = terp.stack.pop
+
+    terp.stack.push list[index]
+  end
+}
+
+ControlWords = {
+  "RUN" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 1
+    list = terp.stack.pop
+    raise Scratch::MissingListExpectation.new(list) unless list.is_a? Array
+
+    terp.interpret terp.make_word( list )
+  end,
+
+  "TIMES" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 2
+    num_times = terp.stack.pop
+    list = terp.stack.pop
+    raise Scratch::MissingListExpectation.new(list) unless list.is_a? Array
+
+    word = terp.make_word list
+    num_times.times do 
+      word.call terp
+    end
+  end,
+
+  "IS_TRUE" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 2
+    code = terp.stack.pop
+    cond = terp.stack.pop
+
+    raise Scratch::MissingListExpectation.new(code) unless code.is_a? Array
+    is_true = cond.call(terp)
+    if is_true
+      terp.interpret terp.make_word(code)
+    end
+  end, 
+
+  "IS_FALSE" => lambda do |terp|
+    terp.error_if_stack_isnt_sufficient! :<, 2
+    list = terp.stack.pop
+    cond = terp.stack.pop
+
+    raise Scratch::MissingListExpectation.new(list) unless list.is_a? Array
+    is_true = cond.call(terp)
+    unless is_true
+      terp.interpret terp.make_word(list)
+    end
+  end
+}
+
+LogicWords = {
+  "TRUE" => lambda {|terp| true }, 
+  "FALSE" => lambda {|terp| false }, 
+}
+
 terp = Scratch.new
 terp.add_words( PrintingWords )
 terp.add_words( MathWords )
@@ -367,9 +474,11 @@ terp.add_words( VariableWords )
 terp.add_words( ConstantWords )
 terp.add_words( StringWords )
 terp.add_words( CommentWords )
-#terp.add_words( FunctionWords )
 terp.add_words( StackWords )
 terp.add_words( CompilingWords )
+terp.add_words( ListWords )
+terp.add_words( ControlWords )
+terp.add_words( LogicWords )
 
 #terp.run "1 2 3 45 678"
 #terp.run "pstack"
@@ -391,8 +500,8 @@ terp.add_words( CompilingWords )
 #terp.run '" ----------------------" print'
 #terp.run "10 pstack"
 #terp.run "1 2 3 print print print"
-terp.run "2 2 + print"
-terp.run "2 2 - print"
+#terp.run "2 2 + print"
+#terp.run "2 2 - print"
 #terp.run "4 2 / print"
 #terp.run "3 3 * 4 4 * + √ print"
 #terp.run "var a"
@@ -409,7 +518,21 @@ terp.run "2 2 - print"
 #terp.run '" Hello World!" print'
 #terp.run '/* abc */ " Hello World!" print'
 #terp.run '/* abc */ Q print'
-terp.run 'def hypot  dup * swap dup * + √  end'
-terp.run '3 4 hypot print'
-#puts terp.stack.inspect
+#terp.run 'def hypot  dup * swap dup * + √  end'
+#terp.run '3 4 hypot .'
+#terp.run '[ 1 2 3 ] .'
+#terp.run '[ 1 2 3 [ 4 5 6 ] 7 ] .'
+#terp.run '[ 1 2 3 [ 4 5 6 ] 7 ] 3 item .'
+#terp.run '[ 1 2 3 ] 1 item .'
+#terp.run '[ 1 " hello" print ] .'
+#terp.run '[ 1 2 3 ] run .'
+#terp.run '[ 1 ! ] 5 times'
+DEBUG=true
+terp.run 'false [ 1 ! ] is_true'
+terp.run 'true [ 3 ! ] is_true'
+terp.run 'false [ 2 ! ] is_false'
+terp.run 'true [ 4 ! ] is_false'
+terp.run 'false [ 5 ! ] is_false'
+terp.run 'false [ 6 ! ] is_false'
+terp.run 'true [ 7 ! ] is_false'
 #puts terp.dictionary.inspect
