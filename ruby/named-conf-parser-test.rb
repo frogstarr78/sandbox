facb4a889e8c38b14f00b52b7ba96c50c35d2e2a
Scott Noel-Hemming <frogstarr78@gmail.com>
2011-05-12 15:07:10 -0700

    Adding named.conf config options also

--- ruby/named-conf-parser-test.rb
+++ ruby/named-conf-parser-test.rb
@@ -3,8 +3,19 @@
 $LOAD_PATH << '.'
 require 'rubygems'
 
+module NamedConf
+  def each_record &block
+    elements.last.elements.each &block
+  end
+end
+
+module Config
+  def record?; false; end
+end
+
 module Comment
   def is_comment?; true; end
+  def record?; false; end
 end
 
 module Record
@@ -17,12 +28,39 @@ module Record
   end
 
   def is_comment?; false; end
+  def record?; true; end
 end
 
 require 'treetop'
 require 'named-conf-parser'
 
 str = %Q(
+options {
+	directory "/var/named";
+	dump-file "/var/named/data/cache_dump.db";
+        statistics-file "/var/named/data/named_stats.txt";
+	allow-query { any; };
+        allow-query-cache { any; };
+	allow-recursion {
+		127.0.0.1;
+		208.77.208.0/24;
+		206.126.21.0/24;
+		64.185.107.177/28;
+		67.158.224.0/24;
+		67.158.225.0/24;
+		208.77.208.0/24;
+		208.77.209.0/24;
+		208.77.210.0/24;
+		208.77.211.0/24;
+		205.210.188.0/24;
+		205.210.189.0/24;
+		205.210.190.0/24;
+		205.210.191.0/24;
+	};
+
+	notify no;
+};
+
 zone "resiliencetrumpsaces.org" in {
         type slave;
         file "slaves/resiliencetrumpsaces.org";
@@ -50,8 +88,8 @@ zone "campbellsvillecc.com" in {
  };
 )
 
-str = File.read '../tmp/life-named.conf'
-str = File.read 'named.conf'
+#str = File.read '../tmp/life-named.conf'
+#str = File.read 'named.conf'
 
 parser = NamedParser.new
 parsed = parser.parse str.gsub /\s*(#|\/\/).*/, ''
@@ -62,8 +100,9 @@ if parsed.nil?
 end
 
 
+#puts parsed.methods.inspect #.elements[2].text_value #each {|rec| rec.text_value }
 #puts parsed.zone_records.elements[2].text_value #each {|rec| rec.text_value }
 parsed.each_record do |item|
-#  puts item.text_value, '-----------'
-  printf "%15s %s\n"% [item.ip, item.domain.text_value]
+#  puts item.text_value, item.methods.sort.inspect, '-----------' if !item.respond_to? :record?
+  printf "%15s %s\n"% [item.ip, item.domain.text_value] #if item.record?
 end
