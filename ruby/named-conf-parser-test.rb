653d6a2a9afbf03dda7a1754622b5e5b7742d870
Scott Noel-Hemming <frogstarr78@gmail.com>
2011-05-13 10:58:51 -0700

    Got the code to parse a full named.conf file

--- ruby/named-conf-parser-test.rb
+++ ruby/named-conf-parser-test.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH << '.'
 require 'rubygems'
+require 'ipaddr'
 
 module NamedConf
   def each_record &block
@@ -19,28 +20,55 @@ module Comment
 end
 
 module Record
+  def type
+    record_options.type_def.elements[2].text_value
+  end
+
+  def file
+    record_options.file_def.file_path.text_value
+  end
+
   def domain
-    quoted_domain.name
+    quoted_name.name
   end
 
   def ip
-    record_options.master_def.raw_ip.text_value
+    if slave?
+      ip = record_options.elements[2].raw_ip.text_value
+    else
+      ip = '208.77.208.2'
+    end
+    @ip ||= IPAddr.new ip, Socket::AF_INET
   end
 
   def is_comment?; false; end
   def record?; true; end
+
+  def master?
+    self.type == 'master'
+  end
+
+  def slave?
+    self.type == 'slave'
+  end
 end
 
 require 'treetop'
 require 'named-conf-parser'
 
 str = %Q(
+// Red Hat BIND Configuration Tool
+// 
+// Default initial "Caching Only" name server configuration
+//
+
 options {
 	directory "/var/named";
 	dump-file "/var/named/data/cache_dump.db";
         statistics-file "/var/named/data/named_stats.txt";
 	allow-query { any; };
         allow-query-cache { any; };
+
 	allow-recursion {
 		127.0.0.1;
 		208.77.208.0/24;
@@ -58,37 +86,47 @@ options {
 		205.210.191.0/24;
 	};
 
+
+	/*
+	 * If there is a firewall between you and nameservers you want
+	 * to talk to, you might need to uncomment the query-source
+	 * directive below.  Previous versions of BIND always asked
+	 * questions using port 53, but BIND 8.1 uses an unprivileged
+	 * port by default.
+	 */
+	 // query-source address * port 53;
 	notify no;
 };
 
-zone "resiliencetrumpsaces.org" in {
-        type slave;
-        file "slaves/resiliencetrumpsaces.org";
-        masters { 208.77.209.177; }; # first ip
+logging {
+	channel query_logging {
+		file "/var/named/log/named_querylog"
+			versions 3 size 100M;
+		print-time yes;			// timestamp log entries
+	};
+	category queries {
+		query_logging;
+	};
+	category lame-servers { null; };
 };
 
-#zone "sullivansouthwrestling.com" in {
-#        type slave;
-#        file "slaves/sullivansouthwrestling.com";
-#        masters { 205.210.189.91; };
-#};
 
-zone "campbellsvillechristianchurch.com" in {
+
+zone "richardbessey.com" in {
         type slave;
-        file "slaves/campbellsvillechristianchurch.com";
-#        masters { 205.210.188.123; };
-        masters { 205.210.188.123; };
-        transfer-source 208.77.208.2;//a comment
+        file "slaves/richardbessey.com";
+        masters { 72.10.12.146; };
  };
 
-zone "campbellsvillecc.com" in {
-        type slave;
-        file "slaves/campbellsvillecc.com"; 
-        masters { 205.210.188.123; };# another comment
- };
+# Reverse DNS files go here
+
+zone "208.77.208.in-addr.arpa" in {
+        type master;
+        file "master/208.77.208.in-addr.arpa";
+};
 )
 
-#str = File.read '../tmp/life-named.conf'
+str = File.read '../tmp/life-named.conf'
 #str = File.read 'named.conf'
 
 parser = NamedParser.new
@@ -104,5 +142,5 @@ end
 #puts parsed.zone_records.elements[2].text_value #each {|rec| rec.text_value }
 parsed.each_record do |item|
 #  puts item.text_value, item.methods.sort.inspect, '-----------' if !item.respond_to? :record?
-  printf "%15s %s\n"% [item.ip, item.domain.text_value] #if item.record?
+  printf "%15s %s\n"% [item.ip, item.domain.text_value] if item.slave?
 end
